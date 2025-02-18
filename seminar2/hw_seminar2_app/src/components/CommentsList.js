d1205fd432e0e6ea69a10caa9df3a9a86cd9a0b4
Bronislav <shevchenko-bg@yandex.ru>
2025-02-18 22:12:53 +0300

    was added hw 2

---
+++ seminar2/hw_seminar2_app/src/components/CommentsList.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const CommentsList = () => {
+	const [comments, setComments] = useState([
+		{ id: 1, text: "Это первый комментарий" },
+		{ id: 2, text: "Это второй комментарий" },
+		{ id: 3, text: "Это третий комментарий" },
+	]);
+
+	const listUpdate = (itemId) => {
+		setComments(comments.filter((item) => item.id !== itemId));
+	};
+
+	console.log(comments);
+
+	return comments.map((item) => (
+		<div key={item.id}>
+			<p>{item.text}</p>
+			<button onClick={() => listUpdate(item.id)}>Удалить</button>
+		</div>
+	));
+};
+
+export default CommentsList;
