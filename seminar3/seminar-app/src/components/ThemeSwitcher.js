ebbdb757670a408035aed653251e6ffba66dfeb9
Bronislav <shevchenko-bg@yandex.ru>
2025-02-21 22:51:17 +0300

    fixed some bugs in seminar 3

--- seminar3/seminar-app/src/components/ThemeSwitcher.js
+++ seminar3/seminar-app/src/components/ThemeSwitcher.js
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import * as React from 'react';
 import Switch from '@mui/material/Switch';
+import PropTypes from "prop-types";
 
 const ThemeSwitcher = () => {
     const [theme, setTheme] = useState("light");
@@ -16,18 +17,24 @@ const ThemeSwitcher = () => {
     const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
     return (
-        <div>
+        <div className="footer_content">
             <Content theme={theme}/>
+            <Switch {...label} defaultChecked onChange={toggleLight}/>
         </div>
     );
 
+
     function Content ({ theme }) {
+
         return (
             <div className="footer_content" style={ {backgroundColor: theme === "light" ? "#aeaeae" : "#454545"} }>
                 <p>Здесть находится какой-то контент с фоном</p>
-                <Switch {...label} defaultChecke onChange={toggleLight} />
             </div>
         );
+    }
+
+    Content.propTypes = {
+        theme: PropTypes.string.isRequired
     }
 
 };
